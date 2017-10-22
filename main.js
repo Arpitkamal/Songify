42d83f328ea90d9b023bf85da728081d4f7ffe28
Arpitkamal <kamalarpit@yahoo.in>
2017-10-23 00:58:52 +0530

    current date is added

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $('.input-wrapper button').on('click ' , function (event) {
     var name = $('#name-input').val();
     $('.main .user-name').html('Walcome, '+name);
     $('.main').removeClass('hidden');
+    var fullDate = new Date();
+    $('.main header .current-date').html(fullDate)
 });
 
 $('.main header>button').on('click' , function () {
