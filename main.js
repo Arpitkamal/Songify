b0bb7c38ddcf33766ac65c36cca4594f4bf502e6
Arpitkamal <kamalarpit@yahoo.in>
2017-10-22 23:58:30 +0530

    audio added

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-$('.input-wrapper>button').on('click ' , function () {
+$('.input-wrapper button').on('click ' , function (event) {
+    event.preventDefault();
     $('.welcome-screen').addClass('hidden');
+    var name = $('#name-input').val();
+    $('.main .user-name').html('Walcome, '+name);
     $('.main').removeClass('hidden');
 });
 
