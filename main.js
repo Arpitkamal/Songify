c4f98e3c0c1092d5e909d356d8b8f2171303ade7
Arpitkamal <kamalarpit@yahoo.in>
2017-10-24 00:15:32 +0530

    song is added and play, pause  added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$('.input-wrapper button').on('click ' , function (event) {
+$('.input-wrapper form').on('submit' , function (event) {
     event.preventDefault();
     $('.welcome-screen').addClass('hidden');
     var name = $('#name-input').val();
@@ -12,4 +12,17 @@ $('.main header>button').on('click' , function () {
    $('.main').addClass('hidden');
    $('.welcome-screen').removeClass('hidden');
 });
-
+                                                // var ispaused=true
+$('.clickable').on('click', function () {
+   var song= $('audio')[0];
+   if(song.paused) {                            // if(ispaused)
+       song.play();
+       $(this).addClass('fa-pause');
+       $(this).removeClass('fa-play');           // ispaused=false
+   }
+   else {
+       song.pause();
+       $(this).addClass('fa-play');
+       $(this).removeClass('fa-pause');           // ispaused=true
+   }
+});
