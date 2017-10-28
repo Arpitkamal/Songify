803a47d95a499fbdb76896523f5db5166d86c6a9
Arpitkamal <kamalarpit@yahoo.in>
2017-10-29 00:09:50 +0530

    song is added with timeduration and current time

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
-function togglemusic() {
-    var song= $('audio')[0];
-    if(song.paused) {                            // if(ispaused)
-        song.play();
-        $('.clickable').addClass('fa-pause').removeClass('fa-play');
-        // ispaused=false
-    }
-    else {
-        song.pause();
-        $('.clickable').addClass('fa-play').removeClass('fa-pause');
-        // ispaused=true
-    }
-}
-
-
-$('.input-wrapper form').on('submit' , function (event) {
-    event.preventDefault();
-    $('.welcome-screen').addClass('hidden');
-    var name = $('#name-input').val();
-    $('.main .user-name').html('Walcome, '+name);
-    $('.main').removeClass('hidden');
-    var fullDate = new Date();
-    $('.main header .current-date').html(fullDate);
-    $(document).on('keypress' , function (event) {
-        if(event.keyCode == 32) {
-            togglemusic();
+$(document).ready(function () {
+    function togglemusic() {
+        var song= $('audio')[0];
+        if(song.paused) {                            // if(ispaused)
+            song.play();
+            $('.clickable').addClass('fa-pause').removeClass('fa-play');
+            // ispaused=false
         }
+        else {
+            song.pause();
+            $('.clickable').addClass('fa-play').removeClass('fa-pause');
+            // ispaused=true
+        }
+    }
+
+    function formattime(time) {
+        const min= Math.floor(time/60);
+        const sec= Math.floor(time%60);
+        return (min < 10 ? '0'+ min : min) +':'+ (sec < 10 ? '0'+sec : sec)
+    }
+
+    function timer() {
+        const song =$('audio')[0];
+        const currenttime = formattime(song.duration);
+        const duration = formattime(song.currentTime);
+        $('.song-duration').text(currenttime);
+        $('.time-elapsed').text(duration);
+    }
+    timer();
+    setInterval(timer, 10000);
+
+    $('.input-wrapper form').on('submit' , function (event) {
+        event.preventDefault();
+        $('.welcome-screen').addClass('hidden');
+        var name = $('#name-input').val();
+        $('.main .user-name').html('Walcome, '+name);
+        $('.main').removeClass('hidden');
+        var fullDate = new Date();
+        $('.main header .current-date').html(fullDate);
+        $(document).on('keypress' , function (event) {
+            if(event.keyCode == 32) {
+                togglemusic();
+            }
+        });
     });
-});
 
 
-$('.clickable').on('click', togglemusic);
-
-
-
-
+    $('.clickable').on('click', togglemusic);
 
 
 //back button
-$('.main header>button').on('click' , function () {
-    $('.main').addClass('hidden');
-    $('.welcome-screen').removeClass('hidden');
+    $('.main header>button').on('click' , function () {
+        $('.main').addClass('hidden');
+        $('.welcome-screen').removeClass('hidden');
+    });
+
 });
