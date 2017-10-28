6213756205542cd373843bf9ea2fb448908b0b3c
Arpitkamal <kamalarpit@yahoo.in>
2017-10-28 13:39:15 +0530

    code is better now

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function togglemusic() {
+    var song= $('audio')[0];
+    if(song.paused) {                            // if(ispaused)
+        song.play();
+        $('.clickable').addClass('fa-pause').removeClass('fa-play');
+        // ispaused=false
+    }
+    else {
+        song.pause();
+        $('.clickable').addClass('fa-play').removeClass('fa-pause');
+        // ispaused=true
+    }
+}
+
+
 $('.input-wrapper form').on('submit' , function (event) {
     event.preventDefault();
     $('.welcome-screen').addClass('hidden');
@@ -5,43 +20,24 @@ $('.input-wrapper form').on('submit' , function (event) {
     $('.main .user-name').html('Walcome, '+name);
     $('.main').removeClass('hidden');
     var fullDate = new Date();
-    $('.main header .current-date').html(fullDate)
-
-
+    $('.main header .current-date').html(fullDate);
+    $(document).on('keypress' , function (event) {
+        if(event.keyCode == 32) {
+            togglemusic();
+        }
+    });
 });
 
+
+$('.clickable').on('click', togglemusic);
+
+
+
+
+
+
+//back button
 $('.main header>button').on('click' , function () {
-   $('.main').addClass('hidden');
-   $('.welcome-screen').removeClass('hidden');
+    $('.main').addClass('hidden');
+    $('.welcome-screen').removeClass('hidden');
 });
-
-$(document).on('keypress' , function (event) {
-    if(event.keyCode == 32) {
-        var song = $('audio')[0];
-        if (song.paused) {
-            song.play();
-            $('.clickable').addClass('fa-pause').removeClass('fa-play');
-        }
-        else {
-            song.pause();
-            $('.clickable').addClass('fa-play').removeClass('fa-pause');
-        }
-    }
-});
-
-
-                                                // var ispaused=true
-$('.clickable').on('click', function () {
-   var song= $('audio')[0];
-   if(song.paused) {                            // if(ispaused)
-       song.play();
-       $(this).addClass('fa-pause').removeClass('fa-play');
-       // ispaused=false
-   }
-   else {
-       song.pause();
-       $(this).addClass('fa-play').removeClass('fa-pause');
-       // ispaused=true
-   }
-});
-
