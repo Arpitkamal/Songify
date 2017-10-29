9bf0d354c7700fcbf6287176a50946341a717f48
Arpitkamal <kamalarpit@yahoo.in>
2017-10-29 17:32:38 +0530

    more songs added

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         }
     }
 
-    $('.song-name').text('zindagi kuch to bata');
+
 
     function formattime(time) {
         const min= Math.floor(time/60);
@@ -28,7 +28,7 @@ $(document).ready(function () {
         $('.time-elapsed').text(duration);
     }
     timer();
-    setInterval(timer, 100000);
+    setInterval(timer, 1000);
 
     $('.input-wrapper form').on('submit' , function (event) {
         event.preventDefault();
@@ -55,4 +55,27 @@ $(document).ready(function () {
         $('.welcome-screen').removeClass('hidden');
     });
 
+    const songlist=['zindagi kush to bata', 'song2', 'song3', 'song4'];
+    for ( var i=0; i<songlist.length; i++) {
+        var index=i+1;
+        $('#song'+index+ ' .song-name').text(songlist[i]);
+    }
+
+    const filename=['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
+    $('#song1').on('click', function () {
+        song.src=filename[0];
+        togglemusic();
+    });
+    $('#song2').on('click', function () {
+        song.src=filename[1];
+        togglemusic()
+    });
+    $('#song3').on('click', function () {
+        song.src=filename[2];
+        togglemusic();
+    });
+    $('#song4').on('click', function () {
+        song.src=filename[3];
+        togglemusic();
+    });
 });
